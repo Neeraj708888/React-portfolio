43a6ce1d5e4c40b988970c1741cd267e14cf6c1f
Neeraj708888 <amck.sharma@gmail.com>
2025-02-26 18:34:12 +0530

    initial commit

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+// Old configuration
+// export default {
+//   plugins: {
+//     tailwindcss: {},  //  enable tailwind css processing
+//     autoprefixer: {}, // Ensure browser compatibility by adding vender prefix 
+//   },
+// };
+
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+};
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import logo from '../assets/logo.jpg'
+import {FaLinkedin} from 'react-icons/fa'
+import {FaGithub} from 'react-icons/fa'
+import {FaWhatsapp} from 'react-icons/fa'
+import {FaInstagram} from 'react-icons/fa'
+import {FaFacebook} from 'react-icons/fa'
+
+const Navbar = () => {
+  return (
+    <nav className='mb-20 flex items-center justify-between py-6'>
+        <div className='flex-shrink-0 items-center'>
+            <img className='mx-2 w-10 rounded-2xl' src={logo} alt="logo"  />
+        </div>
+        <div className='m-8 flex items-center justify-between gap-4 text-2xl'>
+        <a 
+          href="https://www.linkedin.com/in/your-profile" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          aria-label="LinkedIn"
+        >
+        <FaLinkedin />
+        </a>
+        <a 
+          href="https://github.com/Neeraj708888" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          aria-label="GitHub"
+        > 
+        <FaGithub/>
+        </a>
+          <a 
+          href="https://wa.me/7088887210" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          aria-label="Whatsapp"
+        >
+        <FaWhatsapp/>
+        </a>
+        <a 
+          href="https://www.linkedin.com/in/your-profile" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          aria-label="Instagram"
+        >
+        <FaInstagram/>
+        </a>
+        <a 
+          href="https://www.linkedin.com/in/your-profile" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          aria-label="Facebook"
+        >
+        <FaFacebook />
+        </a>
+        </div>
+    </nav>
+  )
+}
+export default Navbar;
+
+// Linkidin: www.linkedin.com/in/neeraj-kumar-9007ba258
+// gitHub: https://github.com/Neeraj708888
+// replit:  https://replit.com/@Neeraj-8888
+// whatsapp:  +91 7088887210
